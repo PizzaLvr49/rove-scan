d32cb7ce0ddc925066f09c94ad101c20a5bcd9ad
PizzaLvr49 <jhtodd2012@gmail.com>
2026-05-17 21:12:10 -0400

    basic network hellow world

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #![no_std]
 #![no_main]
 
+use core::net::Ipv4Addr;
+
 use defmt::*;
 use defmt_rtt as _;
 use panic_probe as _;
 
 use embassy_executor::Spawner;
+use embassy_net::{
+    self, Config as NetConfig, DhcpConfig, IpAddress, StackResources,
+    udp::{PacketMetadata, UdpSocket},
+};
 use embassy_rp::{
     bind_interrupts,
     config::Config,
@@ -15,9 +21,10 @@ use embassy_rp::{
     pio::{self, Pio},
 };
 use embassy_time::{Duration, Timer};
+
 use static_cell::StaticCell;
 
-use cyw43::{SpiBus, aligned_bytes};
+use cyw43::{JoinOptions, NetDriver, SpiBus, aligned_bytes};
 use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
 
 bind_interrupts!(struct Irqs {
@@ -26,11 +33,17 @@ bind_interrupts!(struct Irqs {
 });
 
 static STATE: StaticCell<cyw43::State> = StaticCell::new();
+static RESOURCES: StaticCell<StackResources<4>> = StaticCell::new();
 
 #[embassy_executor::task]
 async fn wifi_task(
     runner: cyw43::Runner<'static, SpiBus<Output<'static>, PioSpi<'static, PIO0, 0>>>,
 ) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, NetDriver<'static>>) -> ! {
     runner.run().await
 }
 
@@ -60,7 +73,7 @@ async fn main(spawner: Spawner) {
 
     let state = STATE.init(cyw43::State::new());
 
-    let (_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
 
     spawner.spawn(unwrap!(wifi_task(runner)));
 
@@ -68,11 +81,52 @@ async fn main(spawner: Spawner) {
         .init(include_bytes!("../firmware/43439A0_clm.bin"))
         .await;
 
-    let mut i = 0;
+    control
+        .set_power_management(cyw43::PowerManagementMode::None)
+        .await;
+
+    control
+        .join("SSID", JoinOptions::new(b"PASS"))
+        .await
+        .unwrap();
+
+    info!("WiFi connected");
+
+    let config = NetConfig::dhcpv4(DhcpConfig::default());
+
+    let (stack, runner) = embassy_net::new(
+        net_device,
+        config,
+        RESOURCES.init(StackResources::new()),
+        0x1234,
+    );
+
+    spawner.spawn(unwrap!(net_task(runner)));
+
+    Timer::after(Duration::from_secs(3)).await;
+
+    let mut rx_meta = [PacketMetadata::EMPTY; 16];
+    let mut tx_meta = [PacketMetadata::EMPTY; 16];
+
+    let mut rx = [0u8; 256];
+    let mut tx = [0u8; 256];
+
+    let mut socket = UdpSocket::new(stack, &mut rx_meta, &mut rx, &mut tx_meta, &mut tx);
+    socket.bind(0).unwrap();
+
+    let msg = b"hello world";
 
     loop {
         Timer::after(Duration::from_secs(1)).await;
-        info!("alive {}", i);
-        i += 1;
+
+        let result = socket
+            .send_to(msg, (IpAddress::Ipv4(Ipv4Addr::new(192, 168, 1, 50)), 9000))
+            .await;
+
+        if let Err(err) = result {
+            warn!("net error: {}", err);
+        }
+
+        info!("sent");
     }
 }
